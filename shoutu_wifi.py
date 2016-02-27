e05ee6819bd9e9f4c0b5b3ce1c808354bc45bca9
kai <okkylesu@gmail.com>
2016-02-27 19:10:04 +0800

    修改：     shoutu_wifi.py         增加密码保存功能

--- shoutu_wifi.py
+++ shoutu_wifi.py
@@ -16,7 +16,6 @@ import os
 import getpass
 
 
-# 如果不想每次输入密码,可在此将 ACCOUNT 和 PASSWORD 预设, 如:
 # ACCOUNT = '000111222333'
 # PASSWORD = '123456'
 ACCOUNT = None
@@ -35,6 +34,10 @@ def login():
     except URLError as e:
         print('请检查网络连接')
         sys.exit()
+    except:
+        print('未知错误,请重试')
+        sys.exit()
+
     if re.search('卡号或密码错误', response):
         print('卡号或密码错误, 请修改后重试')
         sys.exit()
@@ -48,23 +51,48 @@ def check_connect():
     else:
         return True
 
+def save_info(account=None, password=None):
+    try:
+        with open('.userinfo', 'w') as f:
+            userinfo = account, password
+            f.writelines(','.join(userinfo))
+            return True
+    except IOError:
+        print('不能保存密码, 请检测文件访问权限')
+        return False
+    except Exception:
+        sys.exit()
+
+def load_info():
+    try:
+        if os.path.exists('.userinfo'):
+            if os.path.getsize('.userinfo') == 0:
+                return None
+            else:
+                with open('.userinfo', 'r') as f:
+                    userinfo = tuple(f.readline().split(','))
+                    return userinfo
+    except Exception as e:
+        sys.exit()
+
 # main program
 if __name__ == "__main__":
     try:
-
-        if not ACCOUNT or not PASSWORD:
-            if len(sys.argv) == 1:
-                ACCOUNT = input('输入读者卡号: ')
-                PASSWORD = getpass.getpass(prompt='输入六位数密码: ')
-            elif len(sys.argv) == 2:
-                ACCOUNT = sys.argv[1]
-                PASSWORD = getpass.getpass(prompt='输入六位数密码: ')
-            else:
-                ACCOUNT = sys.argv[1]
-                PASSWORD = sys.argv[2]
+        info = load_info()
+        if info:
+            ACCOUNT, PASSWORD = info
+        else:
+            ACCOUNT = input('输入读者卡号: ')
+            PASSWORD = getpass.getpass(prompt='输入六位数密码: ')
+            
 
         if login():
+            if not info:
+                is_save = input('是否保存密码? (y)es/(n)o:').strip().lower() 
+                if is_save == 'y' or is_save == 'yes':
+                    save_info(ACCOUNT, PASSWORD)
             print('连接成功')
+            print('若要退出请按 Ctrl + C ')
         while True:
             state = check_connect()
             if not state:
@@ -73,5 +101,7 @@ if __name__ == "__main__":
                 sleep(30)
     except KeyboardInterrupt as k:
         print('退出程序...')
+        sys.exit()
+    except Exception as e:
         sys.exit()
 
