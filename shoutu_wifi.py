a15fb12718104893d8e788ab8bcc670738756022
kai <okkylesu@gmail.com>
2016-02-27 13:34:38 +0800

    修改：     shoutu_wifi.py

--- shoutu_wifi.py
+++ shoutu_wifi.py
@@ -3,31 +3,42 @@
 
 '''
 usage: 在修改读者卡号和密码后, 直接运行: python3 shoutu_wifi.py
-
 '''
 
 from urllib import parse
 from urllib import request
+from urllib.error import URLError
 from time import sleep
 import re
 import subprocess
+import sys
 import os
-
-ACCOUNT = '000000000000'
-PASSWORD = '100000'
+import getpass
 
 
+# 如果不想每次输入密码,可在此将 ACCOUNT 和 PASSWORD 预设, 如:
+# ACCOUNT = '000111222333'
+# PASSWORD = '123456'
+ACCOUNT = None
+PASSWORD = None
 
 def login():
-    url = 'http://dlpp.clcn.net.cn:1111/login.asp'
+    LOGIN_URL = 'http://dlpp.clcn.net.cn:1111/login.asp'
     userinfo = {
             'username':ACCOUNT,
             'pswd':PASSWORD
             }
     data = parse.urlencode(userinfo).encode('utf8')
-    req = request.Request(url, data)
-    response = request.urlopen(req, timeout=5).read().decode("utf8")
-    return response
+    req = request.Request(LOGIN_URL, data)
+    try:
+        response = request.urlopen(req, timeout=5).read().decode("utf8")
+    except URLError as e:
+        print('请检查网络连接')
+        sys.exit()
+    if re.search('卡号或密码错误', response):
+        print('卡号或密码错误, 请修改后重试')
+        sys.exit()
+    return True
 
 def check_connect():
     fnull = open(os.devnull, 'w')
@@ -40,15 +51,27 @@ def check_connect():
 # main program
 if __name__ == "__main__":
     try:
-        login()
+
+        if not ACCOUNT or not PASSWORD:
+            if len(sys.argv) == 1:
+                ACCOUNT = input('输入读者卡号: ')
+                PASSWORD = getpass.getpass(prompt='输入六位数密码: ')
+            elif len(sys.argv) == 2:
+                ACCOUNT = sys.argv[1]
+                PASSWORD = getpass.getpass(prompt='输入六位数密码: ')
+            else:
+                ACCOUNT = sys.argv[1]
+                PASSWORD = sys.argv[2]
+
+        if login():
+            print('连接成功')
         while True:
             state = check_connect()
-            if state:
-                print('already connected')
-                sleep(60)
-            else:
+            if not state:
                 login()
-                print('connect succeed')
-    except:
-        print('something wrong...')
+            else:
+                sleep(30)
+    except KeyboardInterrupt as k:
+        print('退出程序...')
+        sys.exit()
 
